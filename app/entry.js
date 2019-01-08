dcabaa8f73dc9838fcd8c55ab64f34dd4b433842
show0317sw <tunakan_8584@yahoo.co.jp>
2019-01-09 08:14:54 +0900

    :sparkles: 色も表示するようにした

--- app/entry.js
+++ app/entry.js
@@ -2,11 +2,13 @@
 import $ from 'jquery';
 var socket = io();
 $('form').submit(function(e){
-  e.preventDefault();
-  socket.emit('chat message', $('#m').val());
-  $('#m').val('');
+  if($('#m').val() !== ''){
+    e.preventDefault();
+    socket.emit('chat message', $('#m').val());
+    $('#m').val('');
+  }
   return false;
 });
-socket.on('chat message', function(msg){
-  $('#messages').append($('<li>').text(msg));
+socket.on('chat message', function(msgObj){
+  $('#messages').prepend($('<li>').prepend($(`<div class="color-box" style="background:${msgObj.color}"></div><p>${msgObj.msg}</p>`)));
 });
