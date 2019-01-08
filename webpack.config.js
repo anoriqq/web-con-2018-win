94e27fa85a8225100a327d07b2cdc8d000db2347
show0317sw <tunakan_8584@yahoo.co.jp>
2019-01-09 04:00:13 +0900

    :tada: webpackを設定

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './app/entry.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'public/js')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  }
+};
